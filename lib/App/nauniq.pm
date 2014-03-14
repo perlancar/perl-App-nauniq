2e11a38b0c6456353f4e5c9badc6ffb170b5b7a3
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-03-14 13:29:44 +0700

    Initial commit

---
+++ lib/App/nauniq.pm
@@ -0,0 +1,12 @@
+package App::nauniq;
+
+# VERSION
+
+1;
+# ABSTRACT: Non-adjacent uniq
+
+=head1 SYNOPSIS
+
+See the command-line script L<nauniq>.
+
+=cut
---
+++ lib/App/uappend.pm
@@ -0,0 +1,12 @@
+package App::uappend;
+
+# VERSION
+
+1;
+# ABSTRACT: Append lines to file only if they do not exist yet
+
+=head1 SYNOPSIS
+
+See the command-line script L<uappend>.
+
+=cut
